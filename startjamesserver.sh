8ed1875908ab627f1ce745623ea630c63b5bf5dd
iconx <64724621+iconx2020a@users.noreply.github.com>
2024-12-07 09:32:13 +0100

    Update startjamesserver.sh

--- startjamesserver.sh
+++ startjamesserver.sh
@@ -1,5 +1,7 @@
 #startjamesserver.sh
-#!/bin/bash
+sudo systemctl stop postfix
+sudo systemctl stop smbd
+sudo systemctl stop samba
 cd /opt/james-2.3.2/bin
 echo "apache james server started"
 sudo ./run.sh
