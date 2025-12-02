d99c150d31e5d24e1af5fdd5c9977625a45b72df
iconx <64724621+iconx2020a@users.noreply.github.com>
2025-12-02 17:05:13 +0100

    Add DNS tunneling script using Scapy

---
+++ tcpip/dns_tunnelling.py
@@ -0,0 +1,4 @@
+rom scapy.all import DNS, DNSQR, IP, sr1, UDP
+dns_req = IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname='www.thepacketgeek.com'))
+answer = sr1(dns_req/"data.txt", verbose=0)
+print(answer[DNS].summary())
