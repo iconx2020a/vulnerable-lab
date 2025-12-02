4856cae539bc5ec4bf99c72479ee6cc693695ce2
iconx <64724621+iconx2020a@users.noreply.github.com>
2025-12-02 17:03:51 +0100

    Remove udp_scan function from dup_scan.py

--- tcpip/dup_scan.py
+++ tcpip/dup_scan.py
@@ -1,12 +1,1 @@
-from scapy.all import *
-def udp_scan(target_ip, target_port): 
-  udp_packet = IP(dst=target_ip)/UDP(dport=target_port) 
-  response = sr1(udp_packet, timeout=2, verbose=0) 
-  if response is None: 
-   print("Port "+ str(target_port) + "is open or filtered (UDP Scan)") 
-  elif  response.haslayer(ICMP) and response[ICMP].type == 3 and response[ICMP].code == 3: 
-   print("Port " + str(target_port) + " is closed (UDP Scan)") 
-  else: 
-   print("Port " + str(target_port) + " is filtered (UDP Scan)") 
-if __name__ == "__main__":
-   udp_scan("YourIP", 80)
+
